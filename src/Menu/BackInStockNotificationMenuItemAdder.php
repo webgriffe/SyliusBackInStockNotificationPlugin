afba0da9ef3e926fff995e769d3755a57e863063
Giacomo Moscardini <gmoscardini@webgriffe.com>
2021-02-17 12:08:52 +0100

    [FIX] label in the my account menu

--- src/Menu/BackInStockNotificationMenuItemAdder.php
+++ src/Menu/BackInStockNotificationMenuItemAdder.php
@@ -10,19 +10,10 @@ final class BackInStockNotificationMenuItemAdder
 {
     public function addMenuItems(MenuBuilderEvent $event): void
     {
-        //TODO: make the position configurable by yml
-        $menu = $event->getMenu();
-        //It is not possible to specify the order of an added child so I have to delete the last,
-        //add one and then add the last, to make my item the penultimate
-        $logoutItem = $menu->getLastChild();
-        $menu->removeChild($logoutItem);
-
-        $menu
-            ->addChild('notification_subscription', ['route' => 'app_back_in_stock_notification_account_subscription_book_index'])
-            ->setLabel('app.back_in_stock_notification.account_menu_label')
-            ->setLabelAttribute('icon', 'star')
+        $event->getMenu()
+            ->addChild('notification_subscription', ['route' => 'webgriffe_back_in_stock_notification_account_subscription_book_index'])
+            ->setLabel('webgriffe_bisn.my_account_section.menu_label')
+            ->setLabelAttribute('icon', 'bell')
         ;
-
-        $menu->addChild($logoutItem);
     }
 }
